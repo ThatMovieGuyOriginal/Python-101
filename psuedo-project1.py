7bed34a21ea98576d9992e983dc9bd955ce0c79f
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-05-08 13:06:27 -0600

    Loop correction - extra calls (not needed) / move TEMPS to call once outside loop.

--- psuedo-project1.py
+++ psuedo-project1.py
@@ -6,16 +6,15 @@ MIN_TEMP = 0
 MAX_TEMP = 0
 AVG_TEMP = 0
 
-numTemp = input("What is today's temp?: ")
-
-while COUNTA <= 7:
+while COUNTA < 7:
     numTemp = input("What is today's temp?: ")
+    TEMPS.append(numTemp)
     print(TEMPS[])
     COUNTA = COUNTA + 1
-    MIN_TEMP = min.TEMPS[]
-    MAX_TEMP = max.TEMPS[]
-    AVG_TEMP = avg.TEMPS[]
-    print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
-    numTemp = input("What is today's temp?: ")
+
+MIN_TEMP = min.(TEMPS)
+MAX_TEMP = max.(TEMPS)
+AVG_TEMP = avg.(TEMPS)
+print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
 
 END
