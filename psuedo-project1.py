a4eaa7542128009a9212d6667664d60dec38bd91
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-05-08 13:24:13 -0600

    Updated print statement to be more descriptive.

--- psuedo-project1.py
+++ psuedo-project1.py
@@ -10,4 +10,4 @@ while COUNTA < 7:
 MIN_TEMP = min(TEMPS)
 MAX_TEMP = max(TEMPS)
 AVG_TEMP = sum(TEMPS) / COUNTA
-print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
+print(f"Last week temps - Min: {MIN_TEMP}, Max: {MAX_TEMP}, and Avg: {AVG_TEMP}")
