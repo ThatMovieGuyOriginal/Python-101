69b9ffda68fed9382a0b2e04527dace45a705be1
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-05-08 13:16:17 -0600

    Fix avg(TEMPS) 'sum' and / instead to calculate. Fix unsupported operand for 'int' and 'str', can't mix int and str. Convert numTemp to int when accepting input.

--- psuedo-project1.py
+++ psuedo-project1.py
@@ -2,12 +2,12 @@ TEMPS = []
 COUNTA = 0
 
 while COUNTA < 7:
-    numTemp = input("What is today's temp?: ")
+    numTemp = int(input("What is today's temp?: "))
     TEMPS.append(numTemp)
     print(TEMPS)
     COUNTA = COUNTA + 1
 
 MIN_TEMP = min(TEMPS)
 MAX_TEMP = max(TEMPS)
-AVG_TEMP = avg(TEMPS)
+AVG_TEMP = sum(TEMPS) / COUNTA
 print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
