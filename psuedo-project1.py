e9014b4d51aaaff54722a4f138100687eaf756cc
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-05-08 13:02:07 -0600

    Initial Commit

---
+++ psuedo-project1.py
@@ -0,0 +1,21 @@
+START
+
+LIST = TEMPS[]
+COUNTA = 0
+MIN_TEMP = 0
+MAX_TEMP = 0
+AVG_TEMP = 0
+
+numTemp = input("What is today's temp?: ")
+
+while COUNTA <= 7:
+    numTemp = input("What is today's temp?: ")
+    print(TEMPS[])
+    COUNTA = COUNTA + 1
+    MIN_TEMP = min.TEMPS[]
+    MAX_TEMP = max.TEMPS[]
+    AVG_TEMP = avg.TEMPS[]
+    print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
+    numTemp = input("What is today's temp?: ")
+
+END
