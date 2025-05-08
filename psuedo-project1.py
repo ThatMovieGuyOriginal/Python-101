87169c8bc161ac3ef35a4218cb88bbd0cb03967a
Josh Hickman <joshua.alan.hickman@gmail.com>
2025-05-08 13:11:03 -0600

    Python didn't like new indents... -.- Made TEMPS an empty list with = [].

--- psuedo-project1.py
+++ psuedo-project1.py
@@ -1,17 +1,17 @@
 START
 
-    TEMPS[]
-    COUNTA = 0
+TEMPS = []
+COUNTA = 0
 
-    while COUNTA < 7:
-        numTemp = input("What is today's temp?: ")
-        TEMPS.append(numTemp)
-        print(TEMPS[])
-        COUNTA = COUNTA + 1
+while COUNTA < 7:
+    numTemp = input("What is today's temp?: ")
+    TEMPS.append(numTemp)
+    print(TEMPS[])
+    COUNTA = COUNTA + 1
 
-    MIN_TEMP = min.(TEMPS)
-    MAX_TEMP = max.(TEMPS)
-    AVG_TEMP = avg.(TEMPS)
-    print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
+MIN_TEMP = min.(TEMPS)
+MAX_TEMP = max.(TEMPS)
+AVG_TEMP = avg.(TEMPS)
+print(f"Min: {MIN_TEMP}, Max: {MAX_TEMP}, Avg: {AVG_TEMP}")
 
 END
